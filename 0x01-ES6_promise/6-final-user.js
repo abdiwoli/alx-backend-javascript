8c48e25baad961145c18926231928eb337daa10d
abdiwoli <abdiwolih@gmail.com>
2024-04-24 10:22:30 +0300

    task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
-      result.map((res) => ({
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : String(res.reason),
-      }));
-    });
+    .then((result) => result.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : String(res.reason),
+    })));
 }
