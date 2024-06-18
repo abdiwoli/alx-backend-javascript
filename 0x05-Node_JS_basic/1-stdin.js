48f914706bb1eea18a95e4145adc4610ea4b585f
abdiwoli <abdiwolih@gmail.com>
2024-06-18 20:07:17 +0300

    done

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// command line interact
+const req = require('readline');
+
+const rw = req.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rw.question('Welcome to Holberton School, what is your name?\n', (input) => {
+  console.log(`Your name is:${input}`);
+  console.log('This important software is now closing');
+  rw.close();
+});
