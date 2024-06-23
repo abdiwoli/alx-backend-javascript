9c3ec7ca6d0d7897185d6a1eed5a2db868835ae6
abdiwoli <abdiwolih@gmail.com>
2024-06-23 21:09:49 +0300

    done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 #!/bin/user/node
 const fs = require('readline');
 
+/* eslint-disable comma-dangle */
 const readInterFace = fs.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'Welcome to Holberton School, what is your name?\n'
 
 });
-
+/* eslint-enable comma-dangle */
 const printLine = (data) => {
   console.log(`Your name is: ${data}`);
 };
