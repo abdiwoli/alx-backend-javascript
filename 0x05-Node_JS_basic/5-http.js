0530d0637abaa8a774462ccf2cd4aeb243d951c3
abdiwoli <abdiwolih@gmail.com>
2024-06-25 03:34:54 +0300

    done

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,22 +1,28 @@
+
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (!process.argv[2]) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('This is the list of our students Cannot load the database');
+    }
+    res.write('This is the list of our students\n');
 
-    countStudents('database.csv')
+    countStudents(process.argv[2])
       .then((data) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${data.numStudents}\n`);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         const lines = [];
         for (const [field, firstNames] of Object.entries(data.fieldGroups)) {
           lines.push(`Number of students in ${field}: ${firstNames.length}. List: ${firstNames.join(', ')}`);
         }
+        res.write(`Number of students: ${data.numStudents}\n`);
         res.write(lines.join('\n'));
         res.end();
       })
       .catch(() => {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('This is the list of our students Cannot load the database');
       });
   } else {
