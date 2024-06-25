7c72ba8f7e21ee30e85cdf336eba5c8ffbee2cc5
abdiwoli <abdiwolih@gmail.com>
2024-06-25 14:07:18 +0300

    done

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
@@ -8,7 +9,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-    res.send('This is the list of our students');
+  if (!process.argv[2]) {
+    res.status(404).contentType('text/plain').send('This is the list of our students\nCannot load the database');
+    return;
+  }
+
+  countStudents(process.argv[2])
+    .then((data) => {
+      const lines = [];
+      for (const [field, firstNames] of Object.entries(data.fieldGroups)) {
+        lines.push(`Number of students in ${field}: ${firstNames.length}. List: ${firstNames.join(', ')}`);
+      }
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write(`Number of students: ${data.numStudents}\n`);
+      res.write(lines.join('\n'));
+      res.end();
+    })
+    .catch(() => {
+      res.status(404).contentType('text/plain').send('This is the list of our students\nCannot load the database');
+    });
 });
 
 app.listen(port, () => {
