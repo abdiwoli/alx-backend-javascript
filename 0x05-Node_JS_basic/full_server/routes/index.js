f65c536741104f974bb646821b85772729800ee8
abdwoli <abdiwolih@gmail.com>
2024-07-23 16:25:30 +0300

    taks0

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,9 +1,12 @@
 import AppController from '../controllers/AppController.js';
+import StudentsController from '../controllers/StudentsController.js';
 import express from 'express';
 
 const route = express.Router();
 
 route.get('/', AppController.getHomepage);
+route.get('/students', StudentsController.getAllStudents);
+route.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 
 export default route;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -14,35 +14,30 @@ const parseAsync = (data) => new Promise((resolve, reject) => {
 });
 
 const readDatabase = async (path) => {
-  // Check if the file exists
   if (!fs.existsSync(path)) {
-    return Promise.reject(new Error("File is not accessible"));
+    return Promise.reject(new Error('File is not accessible'));
   }
 
   try {
-    // Read the file data
     const data = await readFileAsync(path, 'utf8');
-    // Parse the CSV data
     const records = await parseAsync(data);
-    // Reduce records into fields
     const fields = records.reduce((obj, val) => {
       const key = val.field;
-      if (!obj[key]) { obj[key] = []; }
+      if (!obj[key]) {
+        // eslint-disable-next-line no-param-reassign
+        obj[key] = [];
+      }
       obj[key].push(val);
       return obj;
     }, {});
-    // Collect lines to return
-    const lines = [];
+    const lines = {};
     for (const [key, records] of Object.entries(fields)) {
-      const namesArray = records.map((el) => el.firstname);
-      const names = namesArray.join(', ');
-      lines.push(`Number of students in ${key}: ${records.length}. List: ${names}`);
+      lines[key] = records.map((el) => el.firstname);
     }
     return lines;
   } catch (error) {
-    // Handle errors
     return Promise.reject(error);
   }
 };
 
-module.exports = readDatabase;
+export default readDatabase;
