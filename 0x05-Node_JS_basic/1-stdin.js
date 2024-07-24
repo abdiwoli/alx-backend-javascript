2b5ad62eb421b017debd8a599f8dee009c6dabb3
abdwoli <abdiwolih@gmail.com>
2024-07-24 07:42:24 +0300

    done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,16 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 console.log('Welcome to Holberton School, what is your name?');
 
-const print = (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-  process.exit();
-};
+readline.question('', (name) => {
+  console.log(`Your name is: ${name.trim()}`);
+  readline.close();
+});
 
-process.stdin.on('data', print);
+if (!process.stdin.isTTY) {
+  console.log('This important software is now closing');
+  process.exit(0);
+}
