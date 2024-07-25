10514082766f9729ca2ebeb0c1b2aeecfe91261c
abdwoli <abdiwolih@gmail.com>
2024-07-25 11:34:57 +0300

    task5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+      consoleLogSpy.restore();
+      if (calculateNumberStub) calculateNumberStub.restore();
+  });
+
+    it('should call Utils.calculateNumber with "SUM", 100, and 20 and log the result', () => {
+            calculateNumberStub = sinon.stub(Utils, 'calculateNumber').callsFake((type, a, b) => a + b);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+      expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+  });
+
+    it('should call Utils.calculateNumber with "SUM", 10, and 10 and log the result', () => {
+            calculateNumberStub = sinon.stub(Utils, 'calculateNumber').callsFake((type, a, b) => a + b);
+    sendPaymentRequestToApi(10, 10);
+
+    // Verify that the stub was called with the correct arguments
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 10, 10)).to.be.true;
+
+      expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+
+  });
+
+    
+});
