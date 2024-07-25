04ed44fbc1461d02637bfa1a418e6027665438b0
abdwoli <abdiwolih@gmail.com>
2024-07-25 07:34:43 +0300

    task1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,34 +3,34 @@ var assert = require('assert');
 
 describe('calculateNumber', () => {
   it('1 + 3 = 4', () => {
-      assert.strictEqual(calculateNumber(1, 3, 'SUM'), 4);
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
   });
 
   it('1 + 4 = 5', () => {
-      assert.strictEqual(calculateNumber(1, 4, 'SUM'), 5);
+      assert.strictEqual(calculateNumber('SUM', 1, 4), 5);
   });
 
   it('1.2 - 3.7 = -2', () => {
-      assert.strictEqual(calculateNumber(1.2, 3.7, "SUBTRACT"), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
   });
 
   it('1.5 - 3.5 = -2', () => {
-      assert.strictEqual(calculateNumber(1.5, 3.5, "SUBTRACT"), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.5), -2);
   });
 
   it('2.4 / 2.4 = 0', () => {
-      assert.strictEqual(calculateNumber(2.4, 2.4, "DIVIDE"), 1);
+      assert.strictEqual(calculateNumber("DIVIDE", 2.4, 2.4), 1);
   });
 
   it('5.5 / 2.5 = 3', () => {
-      assert.strictEqual(calculateNumber(5.5, 2.5, "DIVIDE"), 2);
+      assert.strictEqual(calculateNumber("DIVIDE", 5.5, 2.5), 2);
   });
 
   it('3 + 0.2 = Error', () => {
-      assert.strictEqual(calculateNumber(3, 0.2, "DIVIDE"), 'Error');
+      assert.strictEqual(calculateNumber("DIVIDE", 3, 0.2), 'Error');
   });
 
   it('-1.2 + -3.7 = -5', () => {
-      assert.strictEqual(calculateNumber(-1.2, -3.7, "SUM"), -5);
+      assert.strictEqual(calculateNumber("SUM", -1.2, -3.7), -5);
   });
 });
