b4a97ae45697d766cb157eda28ab2f4907492f8a
abdwoli <abdiwolih@gmail.com>
2024-07-26 05:48:59 +0300

    done

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,13 +1,11 @@
-const express = require('express'); // Correct the import
+const express = require('express');
 
 const app = express();
 
-app.listen(7865, () => console.log('API available on localhost port 7865'));
+const server = app.listen(7865, () => console.log('API available on localhost port 7865'));
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to the payment system');
 });
 
-
-
-module.exports = app;
+module.exports = server;
