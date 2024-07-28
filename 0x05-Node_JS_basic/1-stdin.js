1eed9c532e274faab435c7ce0a8971a492673083
abdwoli <abdiwolih@gmail.com>
2024-07-28 15:48:04 +0300

    fix1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,31 +1,18 @@
-/* eslint-disable comma-dangle */
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Flag to check if the input is piped
-const isPipedInput = !process.stdin.isTTY;
-
 readline.question('', (name) => {
   process.stdout.write(`Your name is: ${name.trim()}\r`);
-
-  if (isPipedInput) {
-    setTimeout(() => {
-      process.stdout.write('This important software is now closing\r');
-      process.exit(0);
-    }, 100);
-  } else {
-    readline.close();
-  }
+  readline.close();
 });
 
-
-process.stdin.on('end', () => {
-  if (isPipedInput) {
+readline.on('close', () => {
+  if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
-    process.exit(0);
   }
+  process.exit(0);
 });
